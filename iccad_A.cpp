3825b5986102f6cfec16af8645f95c7a6cbeb796
KunYu1 <kenny07255270@gmail.com>
2023-07-12 09:30:33 +0800

    Modify reverse map and rewrite the load function

--- iccad_A.cpp
+++ iccad_A.cpp
@@ -22,9 +22,9 @@ map<int, string> logic_map_rev{
 	{2, "or"},
 	{3, "nor"},
 	{4, "xnor"},
-	{5, "xnor"},
-	{6, "xor"},
-	{7, "buf"},
+	{5, "xor"},
+	{6, "buf"},
+	{7, "nand"},
 };
 
 struct Gate {
@@ -173,7 +173,81 @@ string graph2func(RNode *root, vector<string> inputs)
 		
 	return ans;
 }
+Circuit loadCircuit(string file_name){
+    string temp_k;
+    string input_t, temp_t, output_t, wire_t;
+    Circuit circuit;
+    ifstream net_f(file_name);
+    while(net_f>>temp_k){
+        if(temp_k == "input")
+            break;
+    }
+    while(net_f >> input_t>>temp_t){
+        circuit.input.push_back(input_t);
+        if(temp_t == ";")
+            break;
+    }
+    net_f>>temp_k;
+    while(net_f >> output_t>>temp_t){
+        circuit.output.push_back(output_t);
+        if(temp_t == ";")
+            break;
+    }
+    net_f>>temp_k;
+    while(net_f >> wire_t>>temp_t){
+        circuit.wire.push_back(wire_t);
+        if(temp_t == ";")
+            break;
+    }
+    while(net_f >> temp_k){
+        string gate_name="";
+        string input_1="";
+        string input_2="";
+        string output="";
+        if(temp_k == "endmodule")
+            break;
+        switch (logic_map[temp_k])
+        {
+            case 0:
+                net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>temp_k;
+                circuit.gates.push_back(Gate(0,gate_name,input_1,input_2,output));
+                break;
+            case 1:
+                net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
+                circuit.gates.push_back(Gate(1,gate_name,input_1,input_2,output));
+                break;
+            case 2:
+                net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
+                circuit.gates.push_back(Gate(2,gate_name,input_1,input_2,output));
+                break;
+            case 3:
+                net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
+                circuit.gates.push_back(Gate(3,gate_name,input_1,input_2,output));
+                break;
+            case 4:
+                net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
+                circuit.gates.push_back(Gate(4,gate_name,input_1,input_2,output));
+                break;
+            case 5:
+                net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
+                circuit.gates.push_back(Gate(5,gate_name,input_1,input_2,output));
+                break;
+            case 6:
+                net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>temp_k;
+                circuit.gates.push_back(Gate(6,gate_name,input_1,input_2,output));
+                break;
+            case 7:
+                net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
+                circuit.gates.push_back(Gate(7,gate_name,input_1,input_2,output));
+                break;
+            default:
+                break;
+        }
+    }
+    net_f.close();
 
+    return circuit;
+}
 int main() {
     string input_name;
 //    cout << "Please input your input ford name:";
@@ -219,166 +293,22 @@ int main() {
         }
         node_groups_second.push_back(new_group);
     }
-    // cout<<second_net<<endl;
-    // for(Node_group k:node_groups_second){
-    //     cout<<k.n<<" ";
-    //     for(string i:k.nodes)
-    //         cout<<i<<" ";
-    //     cout<<endl;
-    // }
 
-    ifstream first_net_f(input_name+"/"+first_net);
-    ifstream second_net_f(input_name+"/"+second_net);
-    string temp_k;
-    string input_t, temp_t, output_t, wire_t;
-    //first
-    Circuit first_circuit;
-    while(first_net_f>>temp_k){
-        if(temp_k == "input")
-            break;
-    }
-    while(first_net_f >> input_t>>temp_t){
-        first_circuit.input.push_back(input_t);
-        if(temp_t == ";")
-            break;
-    }
-    first_net_f>>temp_k;
-    while(first_net_f >> output_t>>temp_t){
-        first_circuit.output.push_back(output_t);
-        if(temp_t == ";")
-            break;
-    }
-    first_net_f>>temp_k;
-    while(first_net_f >> wire_t>>temp_t){
-        first_circuit.wire.push_back(wire_t);
-        if(temp_t == ";")
-            break;
-    }
-    while(first_net_f >> temp_k){
-        string gate_name="";
-        string input_1="";
-        string input_2="";
-        string output="";
-        if(temp_k == "endmodule")
-            break;
-        switch (logic_map[temp_k])
-        {
-            case 0:
-                first_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>temp_k;
-                first_circuit.gates.push_back(Gate(0,gate_name,input_1,input_2,output));
-                break;
-            case 1:
-                first_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                first_circuit.gates.push_back(Gate(1,gate_name,input_1,input_2,output));
-                break;
-            case 2:
-                first_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                first_circuit.gates.push_back(Gate(2,gate_name,input_1,input_2,output));
-                break;
-            case 3:
-                first_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                first_circuit.gates.push_back(Gate(3,gate_name,input_1,input_2,output));
-                break;
-            case 4:
-                first_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                first_circuit.gates.push_back(Gate(4,gate_name,input_1,input_2,output));
-                break;
-            case 5:
-                first_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                first_circuit.gates.push_back(Gate(5,gate_name,input_1,input_2,output));
-                break;
-            case 6:
-                first_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>temp_k;
-                first_circuit.gates.push_back(Gate(6,gate_name,input_1,input_2,output));
-                break;
-            case 7:
-                first_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                first_circuit.gates.push_back(Gate(7,gate_name,input_1,input_2,output));
-                break;
-            default:
-                break;
-        }
-    }
+    //Load verilog data
+    Circuit first_circuit, second_circuit;
+    first_circuit = loadCircuit(input_name+"/"+first_net);
+    second_circuit = loadCircuit(input_name+"/"+second_net);
 
-    //second
-    Circuit second_circuit;
-    while(second_net_f>>temp_k){
-        if(temp_k == "input")
-            break;
-    }
-    while(second_net_f >> input_t>>temp_t){
-        second_circuit.input.push_back(input_t);
-        if(temp_t == ";")
-            break;
-    }
-    second_net_f>>temp_k;
-    while(second_net_f >> output_t>>temp_t){
-        second_circuit.output.push_back(output_t);
-        if(temp_t == ";")
-            break;
-    }
-    second_net_f>>temp_k;
-    while(second_net_f >> wire_t>>temp_t){
-        second_circuit.wire.push_back(wire_t);
-        if(temp_t == ";")
-            break;
-    }
-    while(second_net_f >> temp_k){
-        string gate_name="";
-        string input_1="";
-        string input_2="";
-        string output="";
-        if(temp_k == "endmodule")
-            break;
-        switch (logic_map[temp_k])
-        {
-            case 0:
-                second_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>temp_k;
-                second_circuit.gates.push_back(Gate(0,gate_name,input_1,input_2,output));
-                break;
-            case 1:
-                second_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                second_circuit.gates.push_back(Gate(1,gate_name,input_1,input_2,output));
-                break;
-            case 2:
-                second_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                second_circuit.gates.push_back(Gate(2,gate_name,input_1,input_2,output));
-                break;
-            case 3:
-                second_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                second_circuit.gates.push_back(Gate(3,gate_name,input_1,input_2,output));
-                break;
-            case 4:
-                second_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                second_circuit.gates.push_back(Gate(4,gate_name,input_1,input_2,output));
-                break;
-            case 5:
-                second_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                second_circuit.gates.push_back(Gate(5,gate_name,input_1,input_2,output));
-                break;
-            case 6:
-                second_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>temp_k;
-                second_circuit.gates.push_back(Gate(6,gate_name,input_1,input_2,output));
-                break;
-            case 7:
-                second_net_f>>gate_name>>temp_k>>output>>temp_k>>input_1>>temp_k>>input_2>>temp_k>>temp_k;
-                second_circuit.gates.push_back(Gate(7,gate_name,input_1,input_2,output));
-                break;
-            default:
-                break;
-        }
-    }
-    
-   
+  
     cout<<endl<<"First Circuit logic gate:"<<endl;
     
     for(Gate g:first_circuit.gates){
-        cout<<g.name<<" "<<g.logic<<" "<<g.input1<<" "<<g.input2<<" "<<g.output<<endl;
+        cout<<g.name<<" "<<logic_map_rev[g.logic]<<" "<<g.input1<<" "<<g.input2<<" "<<g.output<<endl;
     }
 
     cout<<endl<<"Second Circuit logic gate:"<<endl;
     for(Gate g:second_circuit.gates){
-        cout<<g.name<<" "<<g.logic<<" "<<g.input1<<" "<<g.input2<<" "<<g.output<<endl;
+        cout<<g.name<<" "<<logic_map_rev[g.logic]<<" "<<g.input1<<" "<<g.input2<<" "<<g.output<<endl;
     }
     cout<<endl;
     
@@ -395,8 +325,6 @@ int main() {
     	cout<<second_circuit_roots[i]->gate.output<<" = "<<graph2func(second_circuit_roots[i], second_circuit.input)<<endl;
     cout<<endl;
     
-    first_net_f.close();
-    second_net_f.close();
     return 0;
 }
 
